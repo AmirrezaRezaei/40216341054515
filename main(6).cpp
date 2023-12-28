a515316e11a5cccdead8041cd7e677baaf8d3c9d
AmirrezaRezaei <150507166+AmirrezaRezaei@users.noreply.github.com>
2023-12-28 09:15:40 +0330

    Condition
    Condition

---
+++ main(6).cpp
@@ -0,0 +1,18 @@
+// in the name of god
+
+#include <iostream>
+
+using namespace std ;
+int main () {
+
+    int a, b, c, max;
+
+    cin >> a >> b >> c;
+
+    max = a;
+
+    if (b>max) max =b ;
+    if (c>max) max =c ;
+
+    cout << max;
+}
