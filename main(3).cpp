755a9162d7d26c6030eda54575f88cae08839f5f
AmirrezaRezaei <150507166+AmirrezaRezaei@users.noreply.github.com>
2023-12-28 09:12:25 +0330

    Loop
    Loop

---
+++ main(3).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    int i;
+    int j;
+
+    int andazeborj;
+
+
+    cin >> andazeborj;
+
+    for (i = 0; i < andazeborj; i++) {
+        for (j = 0; j <= i; j++) {
+            cout << "*";
+        }
+
+            cout << "\n";
+        }
+
+    }
