4f0b08e656abbab4b7a1429261f139804dd97b17
AmirrezaRezaei <150507166+AmirrezaRezaei@users.noreply.github.com>
2023-12-28 09:14:36 +0330

    Loop 3
    Loop 3

---
+++ main(4).cpp
@@ -0,0 +1,24 @@
+// in the name of god
+
+#include <iostream>
+
+using namespace std ;
+int main ()
+{
+
+    long int a ,sum=0 ;
+    cin >> a ;
+
+    while (a != 0) {
+
+        sum += a;
+        cin >> a;
+    }
+
+    cout << sum;
+
+
+
+
+
+}
