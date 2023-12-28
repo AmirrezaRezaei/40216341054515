63cb0cbd39f5adf648c1bad8ad852ee22fef468d
AmirrezaRezaei <150507166+AmirrezaRezaei@users.noreply.github.com>
2023-12-28 09:34:13 +0330

    Update main(7).cpp

--- main(7).cpp
+++ main(7).cpp
@@ -2,16 +2,15 @@
 using namespace std ;
 int main() {
 
-int m,n,p,f,t;
-cin>>m>>n;
-f=m;
-p=n;
-do{
-t=m%n;
-m=n;
-n=t;
-} while(t!=0);
-cout<<"B.M.M="<<m<<endl;
-cout<<"K.M.M="<<(p*f)/m;
+    int m,n,p,f,t;
+    cin>>m>>n;
+    f=m;
+    p=n;
+    do{
+        t=m%n;
+        m=n;
+        n=t;
+    } while(t!=0);
+    cout<<m<<endl;
+    cout <<(p*f)/m;
 }
-
